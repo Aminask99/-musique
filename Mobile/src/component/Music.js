e2a3030a77dc716eba242e628703c15d5d9d9802
amina <aminasalik012@gmail.com>
2023-06-06 13:57:40 +0100

    ok

---
+++ Mobile/src/component/Music.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function MusicApp() {
+  return (
+    <View>
+        <Text>MusicApp</Text> 
+    </View>
+  )
+}
